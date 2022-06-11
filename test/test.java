4b5363da5ff09338f036e53173cae06090604336
uday_kumar <uday.chintha95@gmail.com>
2022-06-12 01:50:06 +0530

    test

---
+++ test/test.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class test{
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        int number = s.nextInt();
+        System.out.println("test"+number);
+        s.close();
+    }
+}
